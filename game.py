82f05125090f30113cd376078fe8d87397786fb2
ottozinn <ottozinn@icloud.com>
2020-03-03 08:46:08 -0600

    Bleh

---
+++ game.py
@@ -0,0 +1,11 @@
+def intro():
+    name = input("What is your name?")
+    welcome(name)
+
+def welcome(name):
+    print("Welcome, " + name)
+    print("I will pay you 500 dollars if you beat me in this game")
+
+
+# driver
+intro()          
