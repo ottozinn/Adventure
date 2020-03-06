342e028852c04fe5b82c7054db7e309a28b7b4a4
ottozinn <ottozinn@icloud.com>
2020-03-06 08:42:41 -0600

    bleh

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 import random
-global name
+global time
+import sys
+
+sleep = 1
+max = 100
+dice_val = 6
+
 
 def intro():
     global name
@@ -10,16 +16,17 @@ def welcome(name):
     global name
     print("Welcome, " + name)
     print("I will pay you 500 dollars if you beat me in this game")
-
-    
-
+    print("1. Both players start at 0")
+    print("2. If you come to rest at the bottom of a latter, you get to move to the top of it")
+    print("3. If you come to rest on the top of a snak, you have to move down to the bottom of it")
+    print("4. Hit enter to roll the dice")
 def diceRoll():
     min = 1
     max = 6
     roll_again = "yes"
     while roll_again == "yes" or roll_again == "y":
-        print "Rolling the dices..."
-        print "The values are...."
+        print("Rolling the dices...")
+        print("The values are....")
         print random.randint(min, max) 
         roll_again = raw_input("Roll the dice again?")
     if int == 1:
